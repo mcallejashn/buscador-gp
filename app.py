803de404b4b3f65af8626318f7eef41fae4aba45
mcallejashn <miguelnunezh@gmail.com>
2026-06-11 11:23:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Buscador GP", layout="wide")
 
-st.title("🔍 Buscador de Aprendizajes GP")
+st.title("🔍 Biblioteca Digital de Aprendizajes GP")
 
 # Cargar Excel
 df = pd.read_excel("BD Buscador GP.xlsx")
